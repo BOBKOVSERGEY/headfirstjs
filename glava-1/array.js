99df8a9c5e8233a513be37743422c207053d890e
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-05 13:23:29 +0300

    Выводим колличество элементов в массиве

--- glava-1/array.js
+++ glava-1/array.js
@@ -1,5 +1,6 @@
 var flavors = ["vanilla", "butterscotch", "lavender", "chokolate", "cookie dough"];
 flavors[3] = "vanila chokolate chip";
 var flavorOfTheDay = flavors[3];
-
+var numFlovor = flavors.length;
 document.write(flavorOfTheDay);
+document.write('<br>Сколько элементов в массиве: ' + numFlovor);
