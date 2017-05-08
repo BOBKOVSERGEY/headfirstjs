b05c00126cdb37f9b388e8b8b39af2937541a2e5
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-08 18:43:46 +0300

    Почти готовы пузырьки

--- glava-1/arrayReport.js
+++ glava-1/arrayReport.js
@@ -7,9 +7,23 @@ var scores = [60, 50, 60, 58, 54, 54,
 
 var output; // переменная используется в цикле для построения выводимой строки
 
- // переменная для хранения тзекущего индекса
+var highScore = 0;
 
+ // переменная для хранения тзекущего индекса
 for (var i = 0; i < scores.length; i++) {
     output = "Пузырьковый раствор #" + i + " score: " + scores[i] + '<br>';
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+    }
     document.write(output);
 }
+document.write('Самый большой :' + highScore + '<br>');
+
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i);
+    }
+}
+
+document.write('Самый большие :' + bestSolutions);
