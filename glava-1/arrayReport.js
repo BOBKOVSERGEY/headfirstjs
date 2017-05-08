8096b6a3edb7f6ebf7fea487693256c1b51eca35
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-08 19:16:53 +0300

    Готовый скрипт отчета - массивы, функции

--- glava-1/arrayReport.js
+++ glava-1/arrayReport.js
@@ -4,26 +4,60 @@ var scores = [60, 50, 60, 58, 54, 54,
     69, 64, 66, 55, 52, 61,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
+var costs = [.25, .27, .25, .25, .25, .25, .33, .31,
+    .25, .29, .27, .22, .31, .25, .25, .33,
+    .21, .25, .25, .25, .28, .25, .24, .22,
+    .20, .25, .30, .25, .24, .25, .25, .25,
+    .27, .25, .26, .29];
 
-var output; // переменная используется в цикле для построения выводимой строки
+function printAndGetHighScore(scores) {
 
-var highScore = 0;
+    var output; // переменная используется в цикле для построения выводимой строки
 
- // переменная для хранения тзекущего индекса
-for (var i = 0; i < scores.length; i++) {
-    output = "Пузырьковый раствор #" + i + " score: " + scores[i] + '<br>';
-    if (scores[i] > highScore) {
-        highScore = scores[i];
+    var highScore = 0;
+
+    // переменная для хранения тзекущего индекса
+    for (var i = 0; i < scores.length; i++) {
+
+        output = "Пузырьковый раствор #" + i + " score: " + scores[i] + '<br>';
+        document.write(output);
+
+        if (scores[i] > highScore) {
+            highScore = scores[i];
+        }
     }
-    document.write(output);
+    return highScore;
 }
+var highScore = printAndGetHighScore(scores);
+
 document.write('Самый большой :' + highScore + '<br>');
 
-var bestSolutions = [];
-for (var i = 0; i < scores.length; i++) {
-    if (scores[i] == highScore) {
-        bestSolutions.push(i);
+function getBestResults(scores, highScore) {
+    var bestSolutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            bestSolutions.push(i);
+        }
     }
+    return bestSolutions;
 }
+var bestSolutions = getBestResults(scores, highScore);
+document.write('Самый большие :' + bestSolutions + '<br>');
 
-document.write('Самый большие :' + bestSolutions);
+function getMostCostEffectiveSolution(scores, costs, highscore) {
+    var cost = 100; // much higher than any of the costs
+    var index;
+
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == highScore) {
+            if (cost > costs[i]) {
+                index = i;
+                cost = costs[i];
+            }
+        }
+    }
+    return index;
+}
+var getMostCostEffective = getMostCostEffectiveSolution(scores, costs, highScore);
+
+document.write('Самый лучший  :' + getMostCostEffective);
