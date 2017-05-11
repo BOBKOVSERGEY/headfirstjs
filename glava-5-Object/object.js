5158dad3d71520dbc492085e49e3f50bc95f6a88
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-05-11 12:17:56 +0300

    Создание объекта и с его свойств

---
+++ glava-5-Object/object.js
@@ -0,0 +1,10 @@
+// добавление переменнной для хранения объекта
+var chevy = {
+    make: "Chevy",
+    model: "Bel Air",
+    year: 1957,
+    color: "red",
+    passengers: 2,
+    convertible: false,
+    mileage: 1021
+};
