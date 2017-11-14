14df001cacc5baa4b5ccb277c7a7d6903002cbe1
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-11-14 17:50:21 +0300

    Больше замыканий

--- zamykaniya/main.js
+++ zamykaniya/main.js
@@ -13,3 +13,40 @@ var doCount = makeCounter();
 console.log(doCount());
 console.log(doCount());
 console.log(doCount());
+
+function makePassword(password) {
+  return function guess(passwordGues) {
+    return (passwordGues === password);
+  }
+}
+
+var tryGuess = makePassword("secret");
+
+console.log("Guessing 'nope':" + tryGuess('nope'));
+console.log("Guessing 'secret':" + tryGuess('secret'));
+
+
+function multN(n) {
+  return function multBy(m) {
+    return n * m;
+  };
+}
+
+var multBy3 = multN(3);
+console.log('Multiplying 2: ' + multBy3(2));
+console.log('Multiplying 3: ' + multBy3(3));
+
+function makeCounterTwo() {
+  var count = 0;
+  return {
+    increment: function () {
+      count++;
+      return count;
+    }
+  }
+}
+
+var counter = makeCounterTwo();
+console.log(counter.increment());
+console.log(counter.increment());
+console.log(counter.increment());
