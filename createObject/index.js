f1caa335dcbb74da4a1bcfaa2bb933502d00e557
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-01-09 14:10:59 +0300

    Create construct and call object

--- createObject/index.js
+++ createObject/index.js
@@ -1,17 +1,37 @@
-// создаем объект
-/*var dog = {
-  name: "Fido",
-  breed: "Mixed",
-  weight: 38
+function Album(title, artist, year) {
+  this.title = title;
+  this.artist = artist;
+  this.year = year;
+  this.play = function () {
+    console.log(this.title, this.artist, this.year);
+  }
 }
 
-console.log(dog.name);*/
+var darkside = new Album("Dark Side of the Cheese", "Pink Mouse", 1971);
+darkside.play();
+// создаем объект
+/*var dog = {
+ name: "Fido",
+ breed: "Mixed",
+ weight: 38
+ }
+
+ console.log(dog.name);
 
 // создаем конструктор
 function Dog(name, breed, weight) {
   this.name = name;
   this.breed = breed;
   this.weight = weight;
+
+  // добавляем метод
+  this.bark = function () {
+    if (this.weight > 25) {
+      alert(this.name + " says Woof!")
+    } else {
+      alert(this.name + " says Yip!")
+    }
+  }
 }
 
 // использование конструктора создание объекта с помощью конструктора
@@ -24,8 +44,11 @@ var dogs = [fido, fluffy, spot];
 
 // проходимся в цикле по массиву собак
 for (var i = 0; i < dogs.length; i++) {
+  // обращение к методу
+  dogs[i].bark();
+
   var size = "small";
-  if(dogs[i].weight > 10) {
+  if (dogs[i].weight > 10) {
     size = "large";
   }
   console.log("Dog " + dogs[i].name
@@ -33,5 +56,35 @@ for (var i = 0; i < dogs.length; i++) {
     + size + " "
     + dogs[i].breed);
 }
+ */
+/*============================
+// создаем конструктор
+function Coffee(roast, ounces) {
+  this.roast = roast;
+  this.ounces = ounces;
 
+  // метод
+  this.getSize = function () {
+    if (this.ounces === 8) {
+      return 'small';
+    } else if (this.ounces === 12) {
+      return 'medium';
+    }else if (this.ounces === 16) {
+      return 'large';
+    }
+  }
+
+  // 2 метод
+  this.toString = function () {
+    return 'Вы заказали ' + this.getSize() + ' ' + this.roast + ' coffee';
+  }
+}
+
+// создаем объект
+var houseBlend = new Coffee("House Blend", 12);
+
+console.log(houseBlend.toString());
+
+var darkRoast = new Coffee("Dark Roast", 16);
+console.log(darkRoast.toString());*/
 
