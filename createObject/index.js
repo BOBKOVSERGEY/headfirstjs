dcc857851fde218d25451e376ed9400457e548d4
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-01-11 10:55:09 +0300

    Создание конструктора, создание объектного литерала, вызов конструктора

--- createObject/index.js
+++ createObject/index.js
@@ -1,4 +1,83 @@
-function Album(title, artist, year) {
+// контсруктор Car
+function Car(params) {
+  this.make = params.make;
+  this.model = params.model;
+  this.year = params.year;
+  this.color = params.color;
+  this.passengers = params.passengers;
+  this.convertible = params.convertible;
+  this.mileage = params.mileage;
+
+  this.started = false;
+
+  this.start = function () {
+    this.started = true;
+  }
+
+  this.stop = function () {
+    this.started = false;
+  }
+
+  this.drive = function () {
+    if(this.started) {
+      console.log(this.make + ' ' + 'Zoom, Zoom')
+    } else {
+      console.log('You need to start thr engine first');
+    }
+  }
+}
+
+// Создаем объектные литералы
+var taxiParams = {
+  make: "Webville Motors",
+  model: "Taxi",
+  year: 1955,
+  color: "yellow",
+  passengers: 4,
+  convertible: false,
+  mileage: 281341
+};
+var cadiParams = {
+  make: "GM",
+  model: "Cadillac",
+  year: 1955,
+  color: "tan",
+  passengers: 5,
+  convertible: false,
+  mileage: 12892
+};
+var chevyParams = {
+  make: "Chevy",
+  model: "Bel air",
+  year: 1957,
+  color: "red",
+  passengers: 2,
+  convertible: false,
+  mileage: 1021
+};
+
+// вызов конструктора Car
+var cadi = new Car(cadiParams);
+var chevy = new Car(chevyParams);
+var taxi = new Car(taxiParams);
+
+// create array
+var cars = [cadi, chevy, taxi];
+
+// переберем их в цикле
+for(var i = 0; i < cars.length; i++) {
+  cars[i].start();
+  cars[i].drive();
+  cars[i].drive();
+  cars[i].stop();
+}
+
+
+
+/*=============================================================*/
+
+
+/*function Album(title, artist, year) {
   this.title = title;
   this.artist = artist;
   this.year = year;
@@ -8,7 +87,7 @@ function Album(title, artist, year) {
 }
 
 var darkside = new Album("Dark Side of the Cheese", "Pink Mouse", 1971);
-darkside.play();
+darkside.play();*/
 // создаем объект
 /*var dog = {
  name: "Fido",
