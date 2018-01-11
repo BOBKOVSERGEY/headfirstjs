c3ac385c4ca7fbf0973daa6a6e4fb1dafdfbb2f9
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-01-11 11:19:21 +0300

    Проверка к какому конструктору относиться созданный объект instanseof

--- createObject/cardog.js
+++ createObject/cardog.js
@@ -19,7 +19,7 @@ function Car(params) {
   }
 
   this.drive = function () {
-    if(this.started) {
+    if (this.started) {
       console.log(this.make + ' ' + 'Zoom, Zoom')
     } else {
       console.log('You need to start thr engine first');
@@ -53,13 +53,28 @@ var limoParams = {
   convertible: true,
   mileage: 21120
 };
+var cadiParams = {
+  make: "Gm",
+  model: "Cadillac",
+  year: 1955,
+  color: "tan",
+  passengers: 5,
+  convertible: false,
+  mileage: 12892
+};
 
 
 // вызов конструктора Car
 var limo = new Car(limoParams);
 
+var cadi = new Car(cadiParams);
+
 // вызов конструктора Dog
 var limoDog = new Dog('Rhapsody In Blue', 'Poodle', 40);
+
+if (cadi instanceof Car) {
+  console.log("Congrats, it's a Car!");
+}
 
 console.log(limo.make + " " + limo.model + " is a " + typeof limo);
 console.log(limoDog.name + " is a " + typeof limoDog);
